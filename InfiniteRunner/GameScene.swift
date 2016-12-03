52dec743d69c926aba98e0aebd5202d7481e2640
Tamzid Ullah <tamzid257@gmail.com>
2016-12-03 12:07:38 -0500

    added ball functionality

--- InfiniteRunner/GameScene.swift
+++ InfiniteRunner/GameScene.swift
@@ -8,6 +8,7 @@
 
 import SpriteKit
 import GameplayKit
+import CoreMotion
 
 struct CollisionNames {
     static let Hero: UInt32 = 0x1 << 1
@@ -15,6 +16,8 @@ struct CollisionNames {
 }
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    let manager = CMMotionManager()
     
     var gameStarted: Bool = false
     var isAlive: Bool = false
@@ -27,24 +30,45 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     override func didMove(to view: SKView) {
         
+        manager.startAccelerometerUpdates()
+        manager.accelerometerUpdateInterval = 0.1
+        manager.startAccelerometerUpdates(to: OperationQueue.main) {(data,error) in
+            self.physicsWorld.gravity = CGVector(dx: CGFloat((data?.acceleration.x)!) * 10, dy: CGFloat((data?.acceleration.y)!) * 10)
+        }
         let heroTexture = SKTexture(imageNamed: "hero")
         
         hero = SKSpriteNode(texture: heroTexture)
-        hero.position = CGPoint(x: 200, y: 300)
+        hero.position = CGPoint(x: 0, y: 0 )
         hero.name = "Hero"
-        
+        backgroundColor = UIColor.white
         hero.physicsBody = SKPhysicsBody(rectangleOf: hero.size)
-        hero.physicsBody?.affectedByGravity = false
-        hero.physicsBody?.isDynamic = false
+        hero.physicsBody?.affectedByGravity = true
+        
+        hero.physicsBody?.isDynamic = true
+        hero.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
         hero.physicsBody?.categoryBitMask = CollisionNames.Hero
         hero.physicsBody?.contactTestBitMask = CollisionNames.Block
         hero.physicsBody?.collisionBitMask = CollisionNames.Block
         self.addChild(hero)
         
+        let border  = SKPhysicsBody(edgeLoopFrom: self.frame)
+        
+        border.friction = 0
+        border.restitution = 1
+        
+        self.physicsBody = border
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 
+        for touch in touches {
+            let location = touch.location(in: self)
+            hero.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 70))
+            
+        }
+        
+        
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
